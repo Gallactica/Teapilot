9b32f10409f07b8bd3eb7fccbd0f2b1cc9b00856
Gallactica <31005483+Gallactica@users.noreply.github.com>
2024-06-27 16:53:55 +0300

    fix: `container_close` event

--- src/main/java/me/teawin/teapilot/mixin/ClientPlayerEntityMixin.java
+++ src/main/java/me/teawin/teapilot/mixin/ClientPlayerEntityMixin.java
@@ -2,6 +2,7 @@ package me.teawin.teapilot.mixin;
 
 import me.teawin.teapilot.Teapilot;
 import me.teawin.teapilot.TeapilotEvents;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -10,10 +11,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
-    @Inject(method = "closeHandledScreen", at = @At("TAIL"))
+    @Inject(method = "closeHandledScreen", at = @At("HEAD"))
     public void closeHandledScreen(CallbackInfo ci) {
         if (Teapilot.flagsManager.isDisabled("PACKET_CONTAINER")) return;
+        var screen = MinecraftClient.getInstance().currentScreen;
+        if (screen == null) return;
         var event = TeapilotEvents.createEvent(TeapilotEvents.CONTAINER_CLOSE);
+        event.addProperty("side", "client");
         Teapilot.teapilotServer.broadcast(event);
     }
 }
